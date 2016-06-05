b7f13114f228e93c2385824dd292ab04c72fe76c
Paul Jensen <paulbjensen@gmail.com>
2016-06-05 11:24:39 +0100

    Added the default menu module to provide a fuller menu

--- chapter-07/mac-app-menu-electron/app.js
+++ chapter-07/mac-app-menu-electron/app.js
@@ -1,26 +1,8 @@
 'use strict';
 
 var electron = require('electron');
+var defaultMenu = require('electron-default-menu');
 var Menu  = electron.remote.Menu;
-var name = electron.remote.app.getName();
 
-var template = [{
-	label: '', // Gets overridden by Electron
-	submenu: [
-	  {
-	    label: 'About ' + name,
-	    role: 'about'
-	  },
-    {
-      type: 'separator'
-    },
-    {
-      label: 'Quit',
-      accelerator: 'Command+Q',
-      click: electron.remote.app.quit
-    }
-	]
-}];
-
-var menu = Menu.buildFromTemplate(template);
+var menu = Menu.buildFromTemplate(defaultMenu());
 Menu.setApplicationMenu(menu);
