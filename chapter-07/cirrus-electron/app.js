73b4fdc4b76cf966710f599c802a9cab19ea35b0
Paul Jensen <paulbjensen@gmail.com>
2016-06-08 00:18:39 +0100

    Work in progress, will need to sleep

--- chapter-07/cirrus-electron/app.js
+++ chapter-07/cirrus-electron/app.js
@@ -1,123 +1,133 @@
-// 'use strict';
+'use strict';
+
+// Dependencies
 //
-// // Dependencies
-// //
-// var fs 	= require('fs');
-// var gui = require('nw.gui');
-// var currentFile;
-// var content;
-//
-// function openFile () {
-// 	openFileDialog(function (filePath) {
-// 		fs.readFile(filePath, function (err, data) {
-// 			setContent(data);
-// 			hideSelectFileButton();
-// 			showViewMode('design');
-// 		});
-// 	});
-// }
-//
-// function saveFile () {
-// 	fs.writeFile(currentFile, content, function (err) {
-// 		if (err) {
-// 			alert('There was an error');
-// 		}
-// 	});
-// }
-//
-// function loadMenu () {
-//
-// 	var menuBar = new gui.Menu({type:'menubar'});
-//
-// 	// Create sub-menu
-// 	var menuItems = new gui.Menu();
-//
-// 	menuItems.append(new gui.MenuItem({ label: 'Open', click: openFile }));
-// 	menuItems.append(new gui.MenuItem({ label: 'Save', click: saveFile }));
-//
-//
-// 	if (process.platform === 'darwin') {
-//
-// 		// Load Mac OS X application menu
-// 		menuBar.createMacBuiltin('Cirrus');
-//
-// 		menuBar.insert(
-// 		    new gui.MenuItem({
-// 		        label: 'File',
-// 		        submenu: menuItems // menu elements from menuItems object
-// 		    }), 1
-// 		);
-//
-// 	} else {
-//
-// 		// Load Windows/Linux application menu
-// 		menuBar.append(
-// 		    new gui.MenuItem({
-// 		        label: 'File',
-// 		        submenu: menuItems // menu elements from menuItems object
-// 		    }), 1
-// 		);
-//
-// 	}
-//
-// 	gui.Window.get().menu = menuBar;
-//
-// }
-//
-// function openFileDialog (cb) {
-// 	var inputField = document.querySelector('#fileSelector');
-// 	inputField.addEventListener('change', function () {
-// 		var filePath = this.value;
-// 		currentFile = filePath;
-// 		cb(filePath);
-// 	});
-// 	inputField.click();
-// }
-//
-// function bindSelectFileClick (cb) {
-// 	var button = document.querySelector('#openFileView div');
-// 	button.addEventListener('click', function () {
-// 		openFileDialog(cb);
-// 	});
-// }
-//
-// function hideSelectFileButton () {
-// 	var button = document.querySelector('#openFileView');
-// 	button.classList.add('hidden');
-// 	var appView = document.querySelector('#appView');
-// 	appView.classList.remove('hidden');
-// }
-//
-// function showViewMode (viewMode) {
-// 	var areaDivs = window.document.querySelectorAll('.area');
-// 	for (var i=0;i<areaDivs.length;i++) {
-// 		var areaDiv = areaDivs[i];
-// 		areaDiv.classList.add('hidden');
-// 	}
-// 	var selectedArea = window.document.querySelector('#' + viewMode + 'Area');
-// 	selectedArea.classList.remove('hidden');
-// }
-//
-// function setContent (changedContent) {
-// 	if (changedContent) { content = changedContent; }
-// 	var designArea = window.document.querySelector('#designArea');
-// 	designArea.innerHTML = content;
-// 	var codeArea = window.document.querySelector('#codeArea');
-// 	codeArea.value = content;
-// 	var previewArea = window.document.querySelector('#previewArea');
-// 	previewArea.innerHTML = content;
-// }
-//
-// function initialize () {
-// 	bindSelectFileClick(function (filePath) {
-// 		loadMenu();
-// 		fs.readFile(filePath, function (err, data) {
-// 			setContent(data);
-// 			hideSelectFileButton();
-// 			showViewMode('design');
-// 		});
-//
-// 	});
-// }
-//
-// window.onload = initialize;
+var electron 	= require('electron');
+var Menu    	= electron.Menu;
+var ipc     	= electron.ipcRenderer;
+var dialog    = electron.remote.dialog;
+var currentFile;
+var content;
+
+ipc.on('fileRead', function (err, data) {
+	setContent(data);
+	hideSelectFileButton();
+	showViewMode('design');
+});
+
+ipc.on('fileSaved', function (err) {
+	if (err) alert('There was an error');
+});
+
+function openFile () {
+	dialog.showOpenDialog({
+		properties: ['openFile'],
+		filters: {name: 'HTML Files', extensions: ['htm', 'html']}
+	}, function (files) {
+		alert('HAPPENED');
+		alert(files);
+		var file = files[0];
+		console.log(file);
+	});
+
+	// openFileDialog(function (filePath) {
+	// 	ipc.send('readFile', filePath);
+	// });
+}
+
+function saveFile () {
+	ipc.send('saveFile', currentFile, content);
+}
+
+// TODO - replace with Electron's code equivalent for menu items
+function loadMenu () {
+
+	var menuBar = new gui.Menu({type:'menubar'});
+
+	// Create sub-menu
+	var menuItems = new gui.Menu();
+
+	menuItems.append(new gui.MenuItem({ label: 'Open', click: openFile }));
+	menuItems.append(new gui.MenuItem({ label: 'Save', click: saveFile }));
+
+
+	if (process.platform === 'darwin') {
+
+		// Load Mac OS X application menu
+		menuBar.createMacBuiltin('Cirrus');
+
+		menuBar.insert(
+		    new gui.MenuItem({
+		        label: 'File',
+		        submenu: menuItems // menu elements from menuItems object
+		    }), 1
+		);
+
+	} else {
+
+		// Load Windows/Linux application menu
+		menuBar.append(
+		    new gui.MenuItem({
+		        label: 'File',
+		        submenu: menuItems // menu elements from menuItems object
+		    }), 1
+		);
+
+	}
+
+	gui.Window.get().menu = menuBar;
+
+}
+
+function openFileDialog (cb) {
+	var inputField = document.querySelector('#fileSelector');
+	inputField.addEventListener('change', function () {
+		var filePath = this.value;
+		currentFile = filePath;
+		cb(filePath);
+	});
+	inputField.click();
+}
+
+function bindSelectFileClick (cb) {
+	var button = document.querySelector('#openFileView div');
+	button.addEventListener('click', function () {
+		openFileDialog(cb);
+	});
+}
+
+function hideSelectFileButton () {
+	var button = document.querySelector('#openFileView');
+	button.classList.add('hidden');
+	var appView = document.querySelector('#appView');
+	appView.classList.remove('hidden');
+}
+
+function showViewMode (viewMode) {
+	var areaDivs = window.document.querySelectorAll('.area');
+	for (var i=0;i<areaDivs.length;i++) {
+		var areaDiv = areaDivs[i];
+		areaDiv.classList.add('hidden');
+	}
+	var selectedArea = window.document.querySelector('#' + viewMode + 'Area');
+	selectedArea.classList.remove('hidden');
+}
+
+function setContent (changedContent) {
+	if (changedContent) { content = changedContent; }
+	var designArea = window.document.querySelector('#designArea');
+	designArea.innerHTML = content;
+	var codeArea = window.document.querySelector('#codeArea');
+	codeArea.value = content;
+	var previewArea = window.document.querySelector('#previewArea');
+	previewArea.innerHTML = content;
+}
+
+function initialize () {
+	bindSelectFileClick(function (filePath) {
+		//loadMenu();
+		ipc.send('readFile', filePath);
+	});
+}
+
+window.onload = initialize;
--- chapter-07/cirrus-electron/main.js
+++ chapter-07/cirrus-electron/main.js
@@ -3,8 +3,9 @@
 var electron = require('electron');
 var app = electron.app;
 var BrowserWindow = electron.BrowserWindow;
-
+var ipc = electron.ipcMain;
 var mainWindow = null;
+var fs = require('fs');
 
 app.on('window-all-closed', function () {
   if (process.platform !== 'darwin') app.quit();
@@ -14,4 +15,19 @@ app.on('ready', function () {
   mainWindow = new BrowserWindow();
   mainWindow.loadURL('file://' + __dirname + '/index.html');
   mainWindow.on('closed', function () { mainWindow = null; });
+});
+
+function readFile (event, filePath) {
+	fs.readFile(filePath, function (err, data) {
+		ipc.emit('fileRead', err, data);
+	});
+};
+
+// Handles reading the contents of a file
+ipc.on('readFile', readFile);
+
+ipc.on('saveFile', function (currentFile, content) {
+	fs.writeFile(currentFile, content, function (err) {
+		ipc.emite('fileSaved', err);
+	});
 });
