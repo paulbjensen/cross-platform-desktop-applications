2391a3fee053cb9ca9ec69ede584a1dd5be29812
Paul Jensen <paulbjensen@gmail.com>
2016-06-18 19:06:08 +0100

    Applied ES6 styling for the app

---
+++ chapter-01/hello-world-electron/README.md
@@ -0,0 +1,30 @@
+# Hello World (Electron)
+
+A Hello World desktop app, built with Electron for [Cross Platform Desktop Applications](http://manning.com/books/cross-platform-desktop-applications)
+
+### Dependencies
+
+- Node.js (4.x and above)
+- Electron (0.37.x and above)
+
+### Installation
+
+```
+cd PATH_TO_THIS_APP
+npm install
+```
+
+### Starting the app
+
+```
+cd PATH_TO_THIS_APP
+npm start
+```
+
+### About this application
+
+This application was created for [Cross Platform Desktop Applications](http://manning.com/books/cross-platform-desktop-applications)
+
+### Licence and Credits
+
+&copy;2016 Paul Jensen. The app source code is licensed under the MIT License.
--- chapter-01/hello-world-electron/main.js
+++ chapter-01/hello-world-electron/main.js
@@ -1,17 +1,17 @@
 'use strict';
 
-var electron = require('electron');
-var app = electron.app;
-var BrowserWindow = electron.BrowserWindow;
+const electron = require('electron');
+const app = electron.app;
+const BrowserWindow = electron.BrowserWindow;
 
-var mainWindow = null;
+let mainWindow = null;
 
-app.on('window-all-closed', function () {
+app.on('window-all-closed', () => {
   if (process.platform !== 'darwin') app.quit();
 });
 
-app.on('ready', function () {
+app.on('ready', () => {
   mainWindow = new BrowserWindow();
-  mainWindow.loadURL('file://' + __dirname + '/index.html');
-  mainWindow.on('closed', function () { mainWindow = null; });
+  mainWindow.loadURL(`file://${__dirname}/index.html`);
+  mainWindow.on('closed', () => { mainWindow = null; });
 });
