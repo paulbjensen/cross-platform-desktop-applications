6ac49a6351114ee5098abf928721b1518e52dbc6
Paul Jensen <paulbjensen@gmail.com>
2016-06-23 11:59:35 +0100

    Added Let Me Remember Electron app

---
+++ chapter-08/let-me-remember-electron/app.js
@@ -0,0 +1,21 @@
+'use strict';
+
+const electron = require('electron');
+const app = electron.remote.app;
+
+function initialize () {
+	let notes = window.localStorage.notes;
+	if (!notes) notes = 'Let me remember...';
+	window.document.querySelector('textarea').value = notes;
+}
+
+function saveNotes () {
+	let notes = window.document.querySelector('textarea').value;
+	window.localStorage.setItem('notes',notes);
+}
+
+function quit () {
+  app.quit();
+}
+
+window.onload = initialize;
---
+++ chapter-08/let-me-remember-electron/main.js
@@ -0,0 +1,23 @@
+'use strict';
+
+'use strict';
+
+const electron = require('electron');
+const app = electron.app;
+const BrowserWindow = electron.BrowserWindow;
+
+let mainWindow = null;
+
+app.on('window-all-closed', () => {
+  if (process.platform !== 'darwin') app.quit();
+});
+
+app.on('ready', () => {
+  mainWindow = new BrowserWindow({
+    width: 480,
+    height: 320,
+    frame: false
+  });
+  mainWindow.loadURL(`file://${__dirname}/index.html`);
+  mainWindow.on('closed', () => { mainWindow = null; });
+});
