f0045dc30343ad81363808b2807a38337b868294
Paul Jensen <paulbjensen@gmail.com>
2016-06-30 21:32:26 +0100

    Added Watchy Electron

---
+++ chapter-08/watchy-electron/app.js
@@ -0,0 +1,10 @@
+'use strict';
+
+var ipcRenderer =require('electron').ipcRenderer;
+
+function search () {
+	var formInput = window.document.querySelector('form input');
+	var term = formInput.value;
+  ipcRenderer.send('monitorTerm', term);
+	return false;
+}
--- chapter-08/watchy-nwjs/app.js
+++ chapter-08/watchy-nwjs/app.js
@@ -4,10 +4,11 @@ var Twitter = require('twitter');
 var config	= require('./config');
 var term;
 var client = new Twitter(config);
-var gui = window.require('nw.gui');
+
+var notify = chrome.notifications.create;
 
 function notifyOfTweet (tweet) {
-	new gui.Notification('New tweet about ' + term, {
+	notify({
 		icon: tweet.user.profile_image_url,
 		body: tweet.text
 	});
@@ -20,8 +21,8 @@ function search () {
 		alert('Subscribed to stream');
 	  stream.on('data', notifyOfTweet);
 		stream.on('error', function (error) {
-		    alert(error.message);
-	  	});
+		  alert(error.message);
+	  });
 	});
 	return false;
 }
