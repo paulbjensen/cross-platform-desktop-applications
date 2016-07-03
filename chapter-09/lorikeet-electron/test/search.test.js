1a231d255a4869a7da731b78ebb0ef8f96342f09
Paul Jensen <paulbjensen@gmail.com>
2016-07-03 12:48:38 +0100

    Added a unit test for the Lorikeet electron app

---
+++ chapter-09/lorikeet-electron/test/search.test.js
@@ -0,0 +1,45 @@
+'use strict';
+
+const assert	= require('assert');
+const lunr = require('lunr');
+const search = require('../search');
+
+global.window = {};
+global.window.lunr = lunr;
+
+describe('search', () => {
+  describe('#find', () => {
+
+    it('should return results when a file matches a term', (done) => {
+
+    	let seedFileReferences = [
+    		{
+          file: 'john.png',
+          type: 'image/png',
+          path: '/Users/pauljensen/Pictures/john.png'
+        },
+    		{
+          file: 'bob.png',
+          type: 'image/png',
+          path: '/Users/pauljensen/Pictures/bob.png'
+        },
+    		{
+          file: 'frank.png',
+          type: 'image/png',
+          path: '/Users/pauljensen/Pictures/frank.png'
+        }
+    	];
+
+    	search.resetIndex();
+    	seedFileReferences.forEach(search.addToIndex);
+
+    	search.find('frank', (results) => {
+    		assert(results.length === 1);
+    		assert.equal(seedFileReferences[2].path, results[0].ref);
+    		done();
+    	});
+
+    });
+
+  });
+});
