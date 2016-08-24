de3df880afed9b1f18c1889bdbb0c870cc502372
Paul Jensen <paulbjensen@gmail.com>
2016-08-24 22:36:48 +0100

    Adjustments for Chapter 02 final review

--- chapter-02/lorikeet-electron/app.js
+++ chapter-02/lorikeet-electron/app.js
@@ -1,9 +1,9 @@
 'use strict';
 
-var async = require('async');
-var fs = require('fs');
-var osenv = require('osenv');
-var path = require('path');
+const async = require('async');
+const fs = require('fs');
+const osenv = require('osenv');
+const path = require('path');
 
 function getUsersHomeFolder() {
   return osenv.home();
@@ -14,40 +14,51 @@ function getFilesInFolder(folderPath, cb) {
 }
 
 function inspectAndDescribeFile(filePath, cb) {
-  var result = { file: path.basename(filePath), path: filePath, type: '' };
-  fs.stat(filePath, function (err, stat) {
-    if (err) cb(err);
-    if (stat.isFile()) result.type = 'file';
-    if (stat.isDirectory()) result.type = 'directory';
-    cb(err, result);
+  let result = { file: path.basename(filePath), path: filePath, type: '' };
+  fs.stat(filePath, (err, stat) => {
+    if (err) {
+      cb(err);
+    }	else {
+      if (stat.isFile()) {
+        result.type = 'file';
+      }
+      if (stat.isDirectory()) {
+        result.type = 'directory';
+      }
+      cb(err, result);
+    }
   });
 }
 
 function inspectAndDescribeFiles(folderPath, files, cb) {
-  async.map(files, function (file, internalCb) {
-    var resolvedFilePath = path.resolve(folderPath, file);
-    inspectAndDescribeFile(resolvedFilePath, internalCb);
+  async.map(files, (file, asyncCb) => {
+    let resolvedFilePath = path.resolve(folderPath, file);
+    inspectAndDescribeFile(resolvedFilePath, asyncCb);
   }, cb);
 }
 
 function displayFile(file) {
-  var mainArea = document.getElementById('main-area');
-  var template = document.querySelector('#item-template');
-  var clone = document.importNode(template.content, true);
-  clone.querySelector('img').src = 'images/' + file.type + '.svg';
+  const mainArea = document.getElementById('main-area');
+  const template = document.querySelector('#item-template');
+  let clone = document.importNode(template.content, true);
+  clone.querySelector('img').src = `images/${file.type}.svg`;
   clone.querySelector('.filename').innerText = file.file;
   mainArea.appendChild(clone);
 }
 
 function displayFiles(err, files) {
-  if (err) return alert('Sorry, we could not display your files');
+  if (err) {
+    return alert('Sorry, we could not display your files');
+  }
   files.forEach(displayFile);
 }
 
 function main() {
-  var folderPath = getUsersHomeFolder();
-  getFilesInFolder(folderPath, function (err, files) {
-    if (err) return alert('Sorry, we could not load your home folder');
+  let folderPath = getUsersHomeFolder();
+  getFilesInFolder(folderPath, (err, files) => {
+    if (err) {
+      return alert('Sorry, we could not load your home folder');
+    }
     inspectAndDescribeFiles(folderPath, files, displayFiles);
   });
 }
